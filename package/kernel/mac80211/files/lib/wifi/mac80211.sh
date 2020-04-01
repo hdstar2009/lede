1d653014e210db5eb008f57674ec069bb0a4ef33
hdstar2009 <hdstar2009@gmail.com>
2020-04-01 16:23:59 +0800

    Update mac80211.sh

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -119,8 +119,8 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
-			set wireless.default_radio${devidx}.encryption=none
+			set wireless.default_radio${devidx}.ssid=GameRouter
+			set wireless.default_radio${devidx}.encryption=psk-mixed
 EOF
 		uci -q commit wireless
 
